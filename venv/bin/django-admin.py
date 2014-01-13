47f498c7fcba12e0d13caf334030e01d32b22f9f
pradeep <pradeep@pradeep.sharma>
2014-01-13 12:34:02 +0530

    unecessary things removed from git

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/pradeep/projects/python/aws_competition/shareshot/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
